eb6c026e7298aa3d7846a61bc764b098434f5f64
Dodo - Laptop <dominikboros1@gmail.com>
2025-03-14 22:35:25 +0200

    🗺️ Map

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,24 +1,42 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import MapView, {Callout, Marker, PROVIDER_GOOGLE, Region} from 'react-native-maps';
+import {View, Text, StyleSheet, TouchableOpacity, Modal} from 'react-native';
+import MapView, {Region} from 'react-native-maps';
+import * as Location from 'expo-location';
+import {useLocalSearchParams} from "expo-router";
+import AWBModal from "@/scripts/AWBModal";
+
+type RootStackParamList = {
+    final: { awb: string; address: string; status: string; };
+};
+
+const carData: Record<string, { image: string ; description: string }> = {
+    "": {
+        image: "",
+        description: "Description",
+    },
+    "": {
+        image: "",
+        description: "Description",
+    },
+};
 
 type LocationType = {
     latitude: number;
     longitude: number;
     accuracy?: number | null;
 };
-const INITIAL_REGION = {
-    latitude: 45.7494,
-    longitude: 21.2272,
-    latitudeDelta: 0.07,
-    longitudeDelta: 0.07,
-};
 
 const App = () => {
 
+    const params = useLocalSearchParams();
+    const { maker = "", model = "", generation = "", engine = "" } = params;
+    const awbKey = `${maker}_${model}_${generation}_${engine}`;
+    const awbInfo = carData[awbKey];
+
     const mapRef = useRef<any>(null);
-    const [location, setLocation] = useState<LocationType | null>(null);  // Specify the type here
+    const [location, setLocation] = useState<LocationType | null>(null);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
 
     const focusMap = () => {
         if (location && mapRef.current) {
@@ -30,13 +48,42 @@ const App = () => {
             });
         }
     };
+    const [isModalVisible, setModalVisible] = useState(false);
 
-    const onMarkerSelected = (marker: any) => {
-        console.log(marker.name);
+
+    const onClose = () => {
+        setModalVisible(false);
     };
 
     const onRegionChange = (region: Region) => {
         console.log(region);
+    };
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let userLocation = await Location.getCurrentPositionAsync({});
+            setLocation(userLocation.coords);
+
+            mapRef.current.animateToRegion({
+                latitude: userLocation.coords.latitude,
+                longitude: userLocation.coords.longitude,
+                latitudeDelta: 0.02,
+                longitudeDelta: 0.02,
+            });
+        })();
+    }, []);
+
+    const INITIAL_REGION = {
+        latitude: location?.latitude ?? 0,
+        longitude: location?.longitude?? 0,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
     };
 
     return (
@@ -48,12 +95,36 @@ const App = () => {
                     showsUserLocation={!!location}
                     showsMyLocationButton
                     ref={mapRef}
-                    onRegionChangeComplete={onRegionChange}
-                >
+                    onRegionChangeComplete={onRegionChange}>
                 </MapView>
                 <View style={styles.focusButtonContainer}>
+                    <TouchableOpacity style={styles.focusButton} onPress={focusMap}>
+                        <Text style={styles.focusButtonText}>Focus</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.showInfo} onPress={() => setModalVisible(true)}>
+                        <Text style={styles.infoText}>Show Package Info</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
+
+            <TouchableOpacity style={styles.showInfo} onPress={() => setModalVisible(true)}>
+                <Text style={styles.infoText}>Show Package Info</Text>
+            </TouchableOpacity>
+
+            <Modal visible={isModalVisible} transparent={true} animationType="fade">
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalBox}>
+                        <AWBModal
+                            awbInfo={awbInfo}
+                        />
+                        <View style={styles.buttonsContainer}>
+                            <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+                                <Text style={styles.buttonText}>Close</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 };
@@ -61,8 +132,17 @@ const App = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FFF',
-        padding: 20,
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        width: '85%',
+        marginTop: 10,
+    },
+    closeButton: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#8D77AB',
+        borderRadius: 12,
     },
     map: {
         width: '100%',
@@ -74,20 +154,7 @@ const styles = StyleSheet.create({
     },
     mapContainer: {
         width: '100%',
-        height: '90%',
-        backgroundColor: '#d2d5d9',
-        padding: 15,
-        borderRadius: 15,
-    },
-    label: {
-        fontSize: 15,
-        color: '#dadada',
-    },
-    picker: {
-        height: 50,
-        width: 250,
-        backgroundColor: '#616b79',
-        color: '#fff',
+        height: '91%',
     },
     buttonContainer: {
         marginBottom: 30,
@@ -101,12 +168,12 @@ const styles = StyleSheet.create({
     },
     buttonText: {
         fontSize: 20,
-        color: '#fff',
+        color: '#FFF',
     },
     focusButtonContainer: {
         position: 'absolute',
         bottom: 15,
-        left: '55%',
+        left: '43%',
         transform: [{ translateX: -50 }],
         padding: 10,
         borderRadius: 10,
@@ -114,13 +181,43 @@ const styles = StyleSheet.create({
     focusButton: {
         paddingVertical: 10,
         paddingHorizontal: 20,
-        backgroundColor: '#b1abe5',
+        backgroundColor: '#8D77AB',
         borderRadius: 12,
     },
     focusButtonText: {
         color: '#fff',
         fontWeight: 'bold',
         textAlign: 'center',
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalBox: {
+        width: '90%',
+        maxWidth: 400,
+        height: 400,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 3,
+        borderColor: '#8D77AB',
+    },
+    showInfo:{
+        backgroundColor: '#8D77AB',
+        paddingVertical: 8,
+        paddingHorizontal: 8,
+        borderRadius: 12,
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    infoText:{
+        color:"#FFF",
+        fontWeight:"bold",
     },
 });
 
---
+++ scripts/AWBModal.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const AWBModal = ({ awbInfo }) => {
+    return (
+        <View style={styles.modalContainer}>
+            <View style={styles.modalBox}>
+                <Text style={styles.header}>AWB Info2</Text>
+                    <Text>{awbInfo}</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalBoxWithBorder: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: '#6256CA',
+        alignItems: 'center',
+        width: '80%',
+        maxHeight: '50%',
+        justifyContent: 'center',
+    },
+    modalBox: {
+        width: '60%',
+        maxWidth: 400,
+        backgroundColor: 'white',
+        borderRadius: 0,
+        padding: 20,
+        alignItems: 'center',
+    },
+    header: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 15,
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        width: '85%',
+        marginTop: 50,
+
+    },
+    buttonContainer:{
+        backgroundColor: '#6256CA',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        borderRadius: 12,
+        alignItems: 'center',
+        marginBottom: 12,
+        justifyContent: 'flex-end'
+    },
+    picButton:{
+        color: "#FDFAD9",
+    },
+    input: {
+        height: 40,
+        width: 200,
+        borderColor: '#333',
+        borderWidth: 1,
+        borderRadius:8 ,
+        paddingLeft: 15,
+        fontSize: 17,
+    },
+    closeButton: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#8D77AB',
+        borderRadius: 12,
+    }
+});
+
+export default AWBModal;
