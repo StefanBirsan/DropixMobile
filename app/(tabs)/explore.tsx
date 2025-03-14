89d7e4ad60e9a689ef0c38c3a801f2c3a8fd2dde
Dodo - Laptop <dominikboros1@gmail.com>
2025-03-14 19:20:21 +0200

    LogoCommit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -2,13 +2,15 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function TabTwoScreen() {
   return (
-  <View>
-      <Text> Page 2 </Text>
-  </View>
-
-      )
+    <View>
+        <Text> Page 2 </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-
+    Container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    }
 });
