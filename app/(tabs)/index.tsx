449c04553385efe8ef94c87bdc8a5c9a1883400e
Dodo - Laptop <dominikboros1@gmail.com>
2025-03-15 13:39:11 +0200

    👤 Login

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,29 @@
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {Alert, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import CustomButton1 from "@/components/customK/CustomButton1";
-import {useFonts} from "expo-font";
+import ReactNative from "react-native";
+import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from "@firebase/auth";
+import {app, getAuth} from "@/scripts/firebase";
+import {useEffect, useState} from "react";
 
-export default function HomeScreen() {
+const AsyncStorage = require('@react-native-async-storage/async-storage').default;
 
-    let[fontsLoaded] = useFonts({
-        'JetBrainsMono-Medium' : require('@/assets/fonts/JetBrainsMono-Medium.ttf'),
-    })
+type AuthScreenProps = {
+    email: string;
+    setEmail: React.Dispatch<React.SetStateAction<string>>;
+    password: string;
+    setPassword: React.Dispatch<React.SetStateAction<string>>;
+    isLogin: boolean;
+    setIsLogin: React.Dispatch<React.SetStateAction<boolean>>;
+    handleAuthentication: () => void;
+};
+
+type AuthenticatedScreenProps = {
+    user: { name: string; email: string };
+    handleAuthentication: () => void;
+};
+
+
+const AuthScreen = ({ email, setEmail, password, setPassword, isLogin, setIsLogin, handleAuthentication }: AuthScreenProps) =>  {
 
   return (
     <View style={styles.Screen}>
@@ -21,17 +38,154 @@ export default function HomeScreen() {
 
         <View style={styles.LoginForm}>
             <View style={styles.Credentials}>
-                <TextInput placeholder="Username" style={styles.LabelField}/>
-                <TextInput placeholder="Password" secureTextEntry={true} style={styles.LabelField}/>
+                <Text style={styles.leftText}>Email</Text>
+                <TextInput
+                    style={styles.LabelField}
+                    value={email}
+                    onChangeText={setEmail}
+                    autoCapitalize="none"
+                />
+                <Text style={styles.leftText}>Password</Text>
+                <TextInput
+                    style={styles.LabelField}
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
             </View>
-
-            <CustomButton1 text={"Log In"} onPress={() => console.log('clicked')} />
+            <TouchableOpacity style={styles.LoginForm} onPress={handleAuthentication}>
+                <Text>Login</Text>
+            </TouchableOpacity>
         </View>
     </View>
   );
 }
 
+const AuthenticatedScreen = ({ handleAuthentication }: AuthenticatedScreenProps) => {
+
+    const [userInfo, setUserInfo] = useState({
+        name: 'username',
+        input: '',
+    });
+
+    return (
+        <View style={styles.ScreenAuthenticated}>
+            <View style={styles.Header}>
+                <Text style={styles.WelcomeText}>Welcome, (user)!</Text>
+            </View>
+
+            <View style={styles.Body}>
+                <TouchableOpacity style={styles.FooterText} onPress={() => console.log("Scan QR Code")}>
+                    <Text>Scan QR Code</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.FooterText} onPress={() => console.log("Show Package")}>
+                    <Text>Show Packages</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.Footer}>
+                <TouchableOpacity style={styles.FooterText} onPress={handleAuthentication}>
+                    <Text>Log Out</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+
+export default function HomeScreen() {
+
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [user, setUser] = useState<any>(null);
+    const [isLogin, setIsLogin] = useState<boolean>(true);
+
+    const auth = getAuth(app);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+        });
+
+        return () => unsubscribe();
+    }, [auth]);
+
+
+    const handleAuthentication = async () => {
+        try {
+            const auth = getAuth();
+            if (user) {
+                console.log('User logged out successfully!');
+                await signOut(auth);
+            } else {
+                if (isLogin) {
+                    await signInWithEmailAndPassword(auth, email, password);
+                    console.log('User signed in successfully!');
+                } else {
+                    await createUserWithEmailAndPassword(auth, email, password);
+                    console.log('User created successfully!');
+                }
+                setEmail('');
+                setPassword('');
+            }
+        } catch (error) {
+            const e = error as Error;
+            console.error('Authentication error:', e.message);
+            showErrorAlert(e.message);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            {user ? (
+                <AuthenticatedScreen user={user} handleAuthentication={handleAuthentication} />
+            ) : (
+                <AuthScreen
+                    email={email}
+                    setEmail={setEmail}
+                    password={password}
+                    setPassword={setPassword}
+                    isLogin={isLogin}
+                    setIsLogin={setIsLogin}
+                    handleAuthentication={handleAuthentication}
+                />
+            )}
+        </View>
+    );
+}
+
+
+const showErrorAlert = (errorMessage: string | string[]) => {
+    let displayMessage;
+
+    if (errorMessage.includes('auth/invalid-email')) {
+        displayMessage = 'The email address is not valid. Please enter a valid email.';
+    } else if (errorMessage.includes('auth/user-not-found')) {
+        displayMessage = 'No user found with that email. Please sign up first.';
+    } else if (errorMessage.includes('auth/wrong-password')) {
+        displayMessage = 'Incorrect password. Please try again.';
+    } else if (errorMessage.includes('auth/email-already-in-use')) {
+        displayMessage = 'Email already used. Use a different email address.';
+    } else {
+        displayMessage = 'An unknown error occurred. Please try again later.';
+    }
+
+    Alert.alert(
+        'Authentication Failed',
+        displayMessage,
+        [{ text: 'OK' }],
+        { cancelable: true }
+    );
+};
+
+
 const styles = StyleSheet.create({
+    container: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: '#FFF'
+    },
     Screen: {
         height: '100%',
         width: '100%',
@@ -72,12 +226,57 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     LabelField: {
-        fontSize: 18,
+        fontSize: 35,
         backgroundColor: 'white',
         borderRadius: 30,
-        width: '70%',
+        width: '80%',
         textAlign: 'center',
-        borderWidth: 1,
+        borderWidth: 2,
+        margin: 15,
     },
-
+    leftText:{
+        color: '#FFF',
+        left: '-25%',
+    },
+    ScreenAuthenticated: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: "#F9F6E6",
+    },
+    Header: {
+        height: '10%',
+        backgroundColor: "#8D77AB",
+        paddingTop: 20,
+        alignItems: 'center',
+        borderBottomLeftRadius:15,
+        borderBottomRightRadius:15,
+    },
+    WelcomeText: {
+        fontSize: 30,
+        color: 'white',
+        textShadowColor: 'black',
+        textShadowOffset: {width:1, height: 1},
+        textShadowRadius: 4,
+    },
+    Body: {
+        flex: 3,
+        alignItems: "center",
+        justifyContent: "space-evenly",
+    },
+    Footer: {
+        flex: 1,
+        height: '20%',
+        alignItems: "center",
+        justifyContent: "space-evenly",
+    },
+    FooterText:{
+        backgroundColor: '#6256CA',
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        borderRadius: 8,
+        alignItems: 'center',
+        marginBottom: 12,
+        justifyContent: 'flex-end'
+    },
 });
+
